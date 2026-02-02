18230249062f11e254567aaad4ef12752d149ea5
PchelinAndrey <andrej.pchelin@gmail.com>
2026-02-02 15:42:30 +0500

    Create ml_class.py

---
+++ ml_class.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+class ML_model:
+    data_df: None
+    results_df: None
+    metrics_df: None
+
+    def __init__(self):
+        self.data_df = None
+        self.results_df = None
+        self.metrics_df = None
+
+        self.metrics_df = None
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def load_model(self):
+        pass
+
+    def save_model(self):
+        pass
+
+    def save_metrics_and_result(self):
+        pass
+
+    def save_data_df(self):
+        pass
